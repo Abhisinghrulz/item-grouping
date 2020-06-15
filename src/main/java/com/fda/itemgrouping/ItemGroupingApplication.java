e8890d1fd4cb88eae5b64c97218044780d1ff0a1
Abhishek <abhisinghrulz@gmail.com>
2020-06-15 18:25:05 +0530

    Initial Commit

---
+++ src/main/java/com/fda/itemgrouping/ItemGroupingApplication.java
@@ -0,0 +1,13 @@
+package com.fda.itemgrouping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemGroupingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemGroupingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fda/itemgrouping/ItemGroupingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fda.itemgrouping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemGroupingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
